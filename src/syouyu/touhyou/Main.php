ffc0fb02cb3dfdd1f5f6e2e5f4d9d033bdf2a2f8
しょうゆ <65059089+syoooooooooyu@users.noreply.github.com>
2020-06-04 00:44:07 +0900

    Update Main.php

--- src/syouyu/touhyou/Main.php
+++ src/syouyu/touhyou/Main.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ *    ＿＿＿＿＿   _     ＿   ＿　　  ＿　＿　
+ *   /  ＿＿＿/\ //＿＿ | |  | |\  / /|  | |
+ *   \＿＿＿＿\ \// ＿  \ |  | | \/ / |  | |
+ *    ＿＿＿  \  / |＿| | |__| |   /| \__/ |
+ *   /＿＿＿＿/　/\＿＿＿/\_____/  / \_____/
+ *          /＿/            　 /__/
+ *
+ * @author Syouyu(syoooooooooyu)
+ * @link https://github.com/syoooooooooyu/
+ */
+
 namespace syouyu\touhyou;
 
 use pocketmine\command\Command;
